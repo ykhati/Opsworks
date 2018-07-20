86ae622c73d7f48ae7761e0ac89e4e9ca369f45a
Yogesh Khati <yk23888@gmail.com>
2018-07-20 16:33:27 -0700

    remove export

--- jenkins/opsworks_script.js
+++ jenkins/opsworks_script.js
@@ -1,6 +1,4 @@
-export AWS_ACCESS_KEY_ID=AKIAJYMKYWMM5RFF76KQ
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-west-2
+
 
 cd files
 cp /Users/ykhati/Desktop/Gigya/Test/OldTemplates/yogesh-west2-keypair.pem yogesh-west2-keypair.pem
